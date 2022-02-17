9f5fa7d79a7b886cddebf8454b66dd7569c5e66f
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-02-17 13:57:16 +0700

    Use jenkins-spock 2.1.5 on GitHub instead of 2.1.6-SNAPSHOT
    Use snapshot locally, but release on GitHub in order to avoid that Maven
    cannot find the snapshot during the build. At the same time,
    PublishBuildLogIT will be skipped if "jenkins-spock-2.1.5.jar" is found
    on the classpath. I.e., it will be skipped on GitHub, because with that
    buggy dependency it will fail. We can remove that restriction after
    https://github.com/ExpediaGroup/jenkins-spock/pull/111 was merged or the
    corresponding bug fixed otherwise and a new release published.

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q57036137/PublishBuildLogIT.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q57036137/PublishBuildLogIT.groovy
@@ -1,10 +1,16 @@
 package de.scrum_master.stackoverflow.q57036137
 
 import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import spock.lang.IgnoreIf
 import spock.lang.Requires
 
 import static de.scrum_master.testing.JavaAgentDetector.isJMockitActive
 
+@IgnoreIf({
+  // Only works with a locally build 2.1.6-SNAPSHOT, but not on GitHub with 2.1.5
+  // TODO: Bump version after https://github.com/ExpediaGroup/jenkins-spock/pull/111 was merged and released
+  sys["java.class.path"].contains("jenkins-spock-2.1.5.jar")
+})
 @Requires({ JMockitActive })
 class PublishBuildLogIT extends JenkinsPipelineSpecification {
   BuildLogProvider buildLogProvider = Mock()
