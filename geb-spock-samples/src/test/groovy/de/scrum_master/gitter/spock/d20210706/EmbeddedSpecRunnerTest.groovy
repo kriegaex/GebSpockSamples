d54c4814e58d2434d7b85d915f5a38c9b1604a59
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-02-18 16:14:59 +0700

    Skip EmbeddedSpecRunnerTest on JDK 16+
    Groovy 2.5.14 does not support Java 16+ class files, but
    EmbeddedSpecCompiler.loader is assigned a default GroovyClassLoader
    instead of an instance created with a custom CompilerConfiguration.
    Therefore, it seems to default to creating class files with a target
    byte code version of the currently running JDK instead of what it can
    maximally run itself.

--- geb-spock-samples/src/test/groovy/de/scrum_master/gitter/spock/d20210706/EmbeddedSpecRunnerTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/gitter/spock/d20210706/EmbeddedSpecRunnerTest.groovy
@@ -1,9 +1,15 @@
 package de.scrum_master.gitter.spock.d20210706
 
+import spock.lang.Requires
 import spock.lang.Specification
 import spock.util.EmbeddedSpecRunner
 
-class FooTest extends Specification {
+// Groovy 2.5.14 does not support Java 16+ class files, but EmbeddedSpecCompiler.loader is assigned a default
+// GroovyClassLoader instead of an instance created with a custom CompilerConfiguration. Therefore, it seems to default
+// to creating class files with a target byte code version of the currently running JDK instead of what it can maximally
+// run itself.
+@Requires({ Integer.parseInt(System.getProperty("java.version").replaceAll("[.].*", "")) < 16 })
+class EmbeddedSpecRunnerTest extends Specification {
   def test() {
     given:
     EmbeddedSpecRunner specRunner = new EmbeddedSpecRunner()
@@ -13,6 +19,9 @@ class FooTest extends Specification {
       println "### " + myMap['hello']
       System.setProperty("myMap_hello", "world")
     }
+    // We cannot use 'setTargetBytecode' here, because the default compiler config is an anonymous subclass throwing an
+    // exception when setting it.
+    // specRunner.compiler.loader.config.targetBytecode = "8"
     specRunner
       .run """
         import spock.lang.*
