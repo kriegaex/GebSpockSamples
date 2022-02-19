2ae72a7d27e357c287373e10013edc2c9205b937
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-02-19 10:14:29 +0700

    Try to stabilise flaky GitHubTrendingIT

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q59958656/GitHubTrendingIT.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q59958656/GitHubTrendingIT.groovy
@@ -5,22 +5,25 @@ import geb.spock.GebReportingSpec
 class GitHubTrendingIT extends GebReportingSpec {
   def test() {
     given:
-    browser.baseUrl = "https://github.com"
     def providerListPage = to ProviderListPage
     report "provider list page"
+
+    when:
     providerListPage
       .list()
       .collect {
         def pageInfo = it.attr("href").split('/').reverse()
         [pageInfo[1], pageInfo[0]]
       }
+      // Take only the top 3 of 25 elements in order to speed up the test
+      .take(3)
       .each {
         println "trending page = $it"
         to ProviderPage, it[0], it[1]
-        report "trending page"
+        report "trending page ${it[0]}/${it[1]}"
       }
 
-    expect:
+    then:
     true
   }
 }
--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q59958656/ProviderListPage.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q59958656/ProviderListPage.groovy
@@ -3,10 +3,12 @@ package de.scrum_master.stackoverflow.q59958656
 import geb.Page
 
 class ProviderListPage extends Page {
-  static url = "/trending"
+  static url = "https://github.com/trending"
+  static at = { heading == "Trending" && providers.size() >= 10 }
 
   static content = {
-    providers { $(".h3 a") }
+    heading { $("h1.h1").text() }
+    providers(wait: 5) { $(".h3 a") }
   }
 
   def list() {
