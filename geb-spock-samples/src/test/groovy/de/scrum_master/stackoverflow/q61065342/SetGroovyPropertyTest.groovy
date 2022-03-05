9d0eb89ab18e57fbc22c894d0b03afb96419e79e
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-05 10:27:12 +0700

    Check if Groovy 2.5.15 fixes SetGroovyPropertyTest
    On 2.5.14 it has problems as mentioned in the comments below
    https://stackoverflow.com/a/61071840/1082681.

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/SetGroovyPropertyTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/SetGroovyPropertyTest.groovy
@@ -16,13 +16,13 @@ class SetGroovyPropertyTest extends Specification {
     then:
     // Both of these seem to be called sometimes on Groovy 2.5, maybe depending on the OS platform,
     // test order or whatever
-    //1 * collaborator.setX('test')
+    1 * collaborator.setX('test')
     //1 * collaborator.setProperty('x', 'test')
 
     // Try to be as generic as possible, matching both setX and setProperty
-    1 * collaborator./set(X|Property)/(*_) >> { args ->
-      println args
-      assert args.last() == 'test'
-    }
+//    1 * collaborator./set(X|Property)/(*_) >> { args ->
+//      println args
+//      assert args.last() == 'test'
+//    }
   }
 }
