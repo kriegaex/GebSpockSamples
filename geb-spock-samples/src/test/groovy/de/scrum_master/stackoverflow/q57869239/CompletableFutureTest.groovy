4dda644ba23d10415683e07ba6c6bd4f604d7fef
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-05 10:30:06 +0700

    Move CompletableFutureTest to package *.stackoverflow.q57869239

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q57869239/CompletableFutureTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q57869239/CompletableFutureTest.groovy
@@ -5,52 +5,54 @@ import spock.lang.Stepwise
 
 import java.util.concurrent.CompletableFuture
 
-//@Stepwise
+import static java.lang.System.currentTimeMillis
+
+@Stepwise
 class CompletableFutureTest extends Specification {
+  static final int SLEEP_MILLIS = 1
+  static final long TEST_STARTED_AT = currentTimeMillis()
+
   def runnable = Stub(Runnable) {
     run() >> {
-      println "${Date.newInstance()} BEGIN1 in thread ${Thread.currentThread()}"
-      sleep(3000)
-      println "${Date.newInstance()} END1   in thread ${Thread.currentThread()}"
+      printf "%5d BEGIN1 in thread %s%n", currentTimeMillis() - TEST_STARTED_AT, Thread.currentThread()
+      sleep SLEEP_MILLIS
+      printf "%5d END1   in thread %s%n", currentTimeMillis() - TEST_STARTED_AT, Thread.currentThread()
     }
   }
 
   def "runAsyncWithMock"() {
+    given:
+    def startTime = currentTimeMillis()
     when:
     def future = CompletableFuture.runAsync(runnable)
-//    while (!future.done) {
-//      sleep 100
-//    }
-
+    future.get()
     then:
-    true
+    currentTimeMillis() - startTime >= SLEEP_MILLIS
   }
 
   def "runAsyncWithMockAndClosure"() {
+    given:
+    def startTime = currentTimeMillis()
     when:
     def future = CompletableFuture.runAsync({ runnable.run() })
-//    while (!future.done) {
-//      sleep 100
-//    }
-
+    future.get()
     then:
-    true
+    currentTimeMillis() - startTime >= SLEEP_MILLIS
   }
 
   def "runAsyncWithClass"() {
+    given:
+    def startTime = currentTimeMillis()
     when:
     def future = CompletableFuture.runAsync(new Runnable() {
       void run() {
-        println "${Date.newInstance()} BEGIN2 in thread ${Thread.currentThread()}"
-        sleep(3000)
-        println "${Date.newInstance()} END2   in thread ${Thread.currentThread()}"
+        printf "%5d BEGIN2 in thread %s%n", currentTimeMillis() - TEST_STARTED_AT, Thread.currentThread()
+        sleep SLEEP_MILLIS
+        printf "%5d END2   in thread %s%n", currentTimeMillis() - TEST_STARTED_AT, Thread.currentThread()
       }
     })
-//    while (!future.done) {
-//      sleep 100
-//    }
-
+    future.get()
     then:
-    true
+    currentTimeMillis() - startTime >= SLEEP_MILLIS
   }
 }
