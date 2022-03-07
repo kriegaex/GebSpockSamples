ee9585049f6a8be2fca54787d1fde3eb49cfb59a
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-07 09:48:20 +0700

    Add diagnostic logging to SetGroovyPropertyTest
    We also have two feature methods now: One with the problematic case with
    'setX' (ignored on Linux) and one generic one intercepting both 'setX'
    and 'setProperty', logging a stack trace in the stubbed method, so we
    can see what is different on Linux.

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/SetGroovyPropertyTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/SetGroovyPropertyTest.groovy
@@ -1,12 +1,15 @@
 package de.scrum_master.stackoverflow.q61065342
 
+import spock.lang.IgnoreIf
 import spock.lang.Specification
 
 /**
  * https://stackoverflow.com/q/61065342/1082681
  */
 class SetGroovyPropertyTest extends Specification {
-  void 'test'() {
+  // TODO: Fix problem on Linux GitHub CI. Is it Groovy or Spock?
+  @IgnoreIf({ os.linux })
+  void 'expect setX'() {
     setup:
     def collaborator = Mock(Collaborator)
 
@@ -16,12 +19,23 @@ class SetGroovyPropertyTest extends Specification {
     then:
     // Both of these seem to be called sometimes on Groovy 2.5, maybe depending on the OS platform,
     // test order or whatever
-    //1 * collaborator.setX('test')
+    1 * collaborator.setX('test')
     //1 * collaborator.setProperty('x', 'test')
+  }
 
+  void 'expect setX or setProperty'() {
+    setup:
+    def collaborator = Mock(Collaborator)
+
+    when:
+    new UnderTest().call(collaborator, 'test')
+
+    then:
     // Try to be as generic as possible, matching both setX and setProperty
     1 * collaborator./set(X|Property)/(*_) >> { args ->
+      // TODO: Remove log output after problem on Linux GitHub CI has been fixed
       println args
+      new Exception("exception logged for debugging purposes").printStackTrace(System.out)
       assert args.last() == 'test'
     }
   }
