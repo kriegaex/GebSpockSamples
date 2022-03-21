3dbd9269fdf670fc0e883074e557d743b71ee75f
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-22 00:27:38 +0700

    SetGroovyPropertyTest: print Groovy INSTRUCTION_SELECTION debug info
    According to suggestion in
    https://github.com/groovy/groovy-eclipse/issues/1353#issuecomment-1074175065

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
@@ -1,20 +1,22 @@
 package de.scrum_master.stackoverflow.q61065342
 
 import groovy.transform.ASTTest
+import org.codehaus.groovy.classgen.AsmClassGenerator
 
-import static org.codehaus.groovy.control.CompilePhase.CLASS_GENERATION
+import static org.codehaus.groovy.control.CompilePhase.INSTRUCTION_SELECTION
 
 class UnderTest {
-  @ASTTest(phase = CLASS_GENERATION, value = {
-    System.err.println(">> CLASS_GENERATION ----------")
+  @ASTTest(phase = INSTRUCTION_SELECTION, value = {
+    System.err.println(">> INSTRUCTION_SELECTION ----------")
     def expr = node.code.statements[0].expression
     def objectExpression = expr.leftExpression.objectExpression
+    System.err.println(AsmClassGenerator.@setGroovyObjectProperty.@methods*.name)
     System.err.println(objectExpression)
     System.err.println(objectExpression.type)
     System.err.println(objectExpression.originType)
     System.err.println(objectExpression.originType.isDerivedFromGroovyObject())
     System.err.println(objectExpression.getNodeMetaData())
-    System.err.println("<< CLASS_GENERATION ----------")
+    System.err.println("<< INSTRUCTION_SELECTION ----------")
   })
   void call(Collaborator collaborator, String value) {
     collaborator.x = value
