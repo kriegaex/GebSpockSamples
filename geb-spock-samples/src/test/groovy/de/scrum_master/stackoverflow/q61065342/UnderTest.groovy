a679b0abac1d9ac136a9eddc0b270a308c7ad25e
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-21 22:00:25 +0700

    SetGroovyPropertyTest: print debug info during CLASS_GENERATION phase
    Print value of 'type.isDerivedFromGroovyObject()' for Collaborator
    instance. Expected value is 'true' for Windows/MacOS. Under Linux, a
    value of 'false' would confirm the problem in
    https://github.com/groovy/groovy-eclipse/issues/1353.

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
@@ -1,6 +1,15 @@
 package de.scrum_master.stackoverflow.q61065342
 
+import groovy.transform.ASTTest
+
+import static org.codehaus.groovy.control.CompilePhase.CLASS_GENERATION
+
 class UnderTest {
+  @ASTTest(phase = CLASS_GENERATION, value = {
+    def expr = node.code.statements[0].expression
+    def type = expr.leftExpression.objectExpression.type
+    System.err.println("CLASS_GENERATION: type.isDerivedFromGroovyObject() = " + type.isDerivedFromGroovyObject())
+  })
   void call(Collaborator collaborator, String value) {
     collaborator.x = value
   }
