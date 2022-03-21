e02dafb10e6ece43c978ba008132a02354552d75
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-21 23:52:29 +0700

    SetGroovyPropertyTest: print more Groovy CLASS_GENERATION debug info
    According to suggestion in
    https://github.com/groovy/groovy-eclipse/issues/1353#issuecomment-1074133531

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
@@ -6,9 +6,15 @@ import static org.codehaus.groovy.control.CompilePhase.CLASS_GENERATION
 
 class UnderTest {
   @ASTTest(phase = CLASS_GENERATION, value = {
+    System.err.println(">> CLASS_GENERATION ----------")
     def expr = node.code.statements[0].expression
-    def type = expr.leftExpression.objectExpression.type
-    System.err.println("CLASS_GENERATION: type.isDerivedFromGroovyObject() = " + type.isDerivedFromGroovyObject())
+    def objectExpression = expr.leftExpression.objectExpression
+    System.err.println(objectExpression)
+    System.err.println(objectExpression.type)
+    System.err.println(objectExpression.originType)
+    System.err.println(objectExpression.originType.isDerivedFromGroovyObject())
+    System.err.println(objectExpression.getNodeMetaData())
+    System.err.println("<< CLASS_GENERATION ----------")
   })
   void call(Collaborator collaborator, String value) {
     collaborator.x = value
