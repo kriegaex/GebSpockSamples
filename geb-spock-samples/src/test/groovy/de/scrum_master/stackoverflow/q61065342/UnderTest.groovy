c33cc997ec598750f11324384a5a933b1a8cabfd
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-22 00:54:51 +0700

    SetGroovyPropertyTest: change Groovy INSTRUCTION_SELECTION debug info
    Print 'objectExpresison.originType.interfaces', according to suggestion
    in https://github.com/groovy/groovy-eclipse/issues/1353#issuecomment-1074223253

--- geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
+++ geb-spock-samples/src/test/groovy/de/scrum_master/stackoverflow/q61065342/UnderTest.groovy
@@ -10,12 +10,13 @@ class UnderTest {
     System.err.println(">> INSTRUCTION_SELECTION ----------")
     def expr = node.code.statements[0].expression
     def objectExpression = expr.leftExpression.objectExpression
-    System.err.println(AsmClassGenerator.@setGroovyObjectProperty.@methods*.name)
-    System.err.println(objectExpression)
-    System.err.println(objectExpression.type)
-    System.err.println(objectExpression.originType)
-    System.err.println(objectExpression.originType.isDerivedFromGroovyObject())
-    System.err.println(objectExpression.getNodeMetaData())
+//    System.err.println(AsmClassGenerator.@setGroovyObjectProperty.@methods*.name)
+//    System.err.println(objectExpression)
+//    System.err.println(objectExpression.type)
+//    System.err.println(objectExpression.originType)
+//    System.err.println(objectExpression.originType.isDerivedFromGroovyObject())
+    System.err.println(objectExpression.originType.interfaces)
+//    System.err.println(objectExpression.getNodeMetaData())
     System.err.println("<< INSTRUCTION_SELECTION ----------")
   })
   void call(Collaborator collaborator, String value) {
